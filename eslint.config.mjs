abf3e95ec818e7078589ae8a3a61d969aac33c5a
la-rojas13 <superzeus7@gmail.com>
2026-01-13 17:44:53 -0500

    update lint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,7 @@ const eslintConfig = defineConfig([
   ]),
   {
     rules:{
-            "@typescript-eslint/no-unused-vars": "error",
+            //"@typescript-eslint/no-unused-vars": "error",
 
     }
   }
